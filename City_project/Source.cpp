7cf48654c19ef568b752de727c3622d0dde1a1e5
Wojtek Mazur <zgnitek123.sbb@gmail.com>
2016-11-19 19:53:00 +0100

    Sratatatatata

--- City_project/Source.cpp
+++ City_project/Source.cpp
@@ -2,6 +2,9 @@
 
 int main()
 {
+
+	// Komentarz testowy 
+
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
